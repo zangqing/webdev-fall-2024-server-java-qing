5a62c436313c607b23b13f2e56ce8fd69facf595
yaomingxinlu-local <zangqing1990@gmail.com>
2024-10-31 14:50:27 -0700

    first commit

---
+++ src/main/java/com/example/webdev_fall_2024_server_java_qing/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdev_fall_2024_server_java_qing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webdev_fall_2024_server_java_qing/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdev_fall_2024_server_java_qing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
